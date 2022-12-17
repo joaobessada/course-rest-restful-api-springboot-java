e689b3197d79469b80ad50d84e6f75e24e64e48e
joaobessada <joaobessada@gmail.com>
2022-12-17 04:00:16 -0300

    17 - custom exceptions and credentials VO

--- 16_CORS/rest-springboot-java-13/src/main/java/com/bessada/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ 16_CORS/rest-springboot-java-13/src/main/java/com/bessada/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.bessada.exceptions.ExceptionResponse;
+import com.bessada.exceptions.InvalidJwtAuthenticationException;
 import com.bessada.exceptions.RequiredObjectIsNullException;
 import com.bessada.exceptions.ResourceNotFoundException;
 
@@ -30,10 +31,17 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
 	}
+	
 	@ExceptionHandler(RequiredObjectIsNullException.class)
 	public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(Exception ex, WebRequest request) {
 		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(InvalidJwtAuthenticationException.class)
+	public final ResponseEntity<ExceptionResponse> handleInvalidJwtAuthenticationException(Exception ex, WebRequest request) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.FORBIDDEN);
 	}
 	
 	
